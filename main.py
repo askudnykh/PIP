61580239b32bb790cfb04e734d59e62ec5eb1a49
Anton Skudnyh <askudnykh@list.ru>
2023-07-01 13:23:33 +0500

    1 commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from application.salary import calculate_salary
+from application.db.people import get_employees
+from datetime import datetime
+import math22
+
+
+
+print(datetime.date(datetime.today()))
+
+if __name__ == '__main__':
+    calculate_salary()
+    get_employees()
